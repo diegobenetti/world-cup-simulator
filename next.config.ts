3970c3cb9802c8bd38a342dd313c2b4ff0f7f325
Diego Benetti <benetti.benkenstein@gmail.com>
2026-06-21 13:35:32 -0300

    Applies next setting and creates github actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/world-cup-simulator",
 };
 
 export default nextConfig;
